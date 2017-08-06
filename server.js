0d7f86c5b518652a6461c5bc62ce1b34086bbe9a
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-08-05 21:44:21 -0700

    Commit working application with full CRUD

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 console.log("This worked.");
 
 const express = require('express');
-const bodyParser= require('body-parser');
 const app = express();
-
+const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(bodyParser.json())
-
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
 
 var db
 
@@ -24,26 +16,23 @@ MongoClient.connect('mongodb://atomc:tutorial1@ds021434.mlab.com:21434/commentbo
   })
 })
 
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   db.collection('quotes').find().toArray((err, result) => {
    if (err) return console.log(err)
-   // renders index.ejs
    res.render('index.ejs', {quotes: result})
  })
 })
 
 app.post('/quotes', (req, res) => {
-  console.log('Here we are at quotes', req.body);
   db.collection('quotes').save(req.body, (err, result) => {
     if (err) return console.log(err)
-
     console.log('saved to database')
     res.redirect('/')
-  })
-
-  db.collection('quotes').find().toArray(function(err, results) {
-    console.log(results)
   })
 })
 
@@ -60,5 +49,14 @@ app.put('/quotes', (req, res) => {
   }, (err, result) => {
     if (err) return res.send(err)
     res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete(
+  {name: req.body.name},
+  (err, result) => {
+    if (err) return res.send(500, err)
+    res.send({message: 'A darth quote was deleteth.'})
   })
 })
