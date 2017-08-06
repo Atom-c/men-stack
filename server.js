837f4d4a8db5c10dc151f90df65ce120a9346d5c
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-08-05 17:35:24 -0700

    server respond to requests

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log("This worked.");
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
